5b6b78d28fa402b030da32ae0217ef94e332f797
WilderGitHub <wilder_serdan@hotmail.com>
2021-04-21 15:21:19 -0400

    se añadió moneda por requerimiento del Scrum master Juankiller

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import re
 import os
-garchivo = "data/TRNBCB_2017_03.txt"
+garchivo = "data/TRNBCB_ABRIL_2020.txt"
 ''' print(os.getcwd()) '''
 # para sacar el nombrecito del archivo excel oe
 nombrecito = garchivo[-11:-4]
@@ -23,14 +23,14 @@ data = {
     'Anio': dfGarchivo['prueba'].str[20:22],
     'Mes': dfGarchivo['prueba'].str[22:24],
     'Dia': dfGarchivo['prueba'].str[24:26],
-    # 'Moneda': dfGarchivo['prueba'].str[26:28],
+    'Moneda': dfGarchivo['prueba'].str[26:28],
     'Importe': dfGarchivo['prueba'].str[-16:],
 }
 dfPatrabajar = pd.DataFrame(data)
 dfPatrabajar['Importe'] = pd.to_numeric(
     dfPatrabajar['Importe'], errors='coerce')
 Resumen = dfPatrabajar.groupby(
-    ['Sucursal', 'TipoTarjeta', 'Anio', 'Mes', 'Dia']).agg({'Importe': ['sum', 'count', 'median', 'mean', 'min', 'max']})
+    ['Sucursal', 'TipoTarjeta','Moneda', 'Anio', 'Mes', 'Dia']).agg({'Importe': ['sum', 'count', 'median', 'mean', 'min', 'max']})
 with pd.ExcelWriter(nombrecito + '.xlsx') as writer:  # pylint: disable=abstract-class-instantiated
     Resumen.to_excel(writer, sheet_name='Resumen') 
 print(dfPatrabajar.size)
@@ -39,12 +39,9 @@ print(dfPatrabajar.size)
 # explorasps
 #### dfPatrabajar.to_excel(nombrecito + '.xlsx')  
 
-
 ######vamos a tratar de hacer el grafico con matplolib oe
-
-# Primero transformamos a dataframe el groupby
+# Primero transformamos a dataframe el groupby con el resetindex
 palgrafico=dfPatrabajar.groupby(['Dia'])['Importe'].sum().reset_index(name='ElImporte')
-#print(palgrafico.head())
 # convertimos a numpy series:
 diadia=palgrafico['Dia']
 importeimporte=palgrafico['ElImporte']
