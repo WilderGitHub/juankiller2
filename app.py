bc51a64c4df0d53c1206268370792079cd7d034e
WilderGitHub <wilder_serdan@hotmail.com>
2021-04-30 11:34:35 -0400

    para hacer la prueba con el Juankiller

--- app.py
+++ app.py
@@ -11,9 +11,10 @@ nombrecito = garchivo[-11:-4]
 
 dfGarchivo = pd.read_csv(garchivo, sep='\t', names=['prueba'])
 dfGarchivo['prueba'] = dfGarchivo['prueba'].str.replace(",", ".")
-dfGarchivo.drop(
+#con esto borro los cero, pero juankiller no, Ver con el
+""" dfGarchivo.drop(
     dfGarchivo[dfGarchivo['prueba'].str[-16:] == '0000000000000.00'].index, inplace=True)
-''' print(dfGarchivo.head(10)) '''
+''' print(dfGarchivo.head(10)) ''' """
 data = {
     # 'Emisor': dfGarchivo['prueba'].str[9:12],
     # 'Adquiriente': dfGarchivo['prueba'].str[12:15],
@@ -30,7 +31,10 @@ dfPatrabajar = pd.DataFrame(data)
 dfPatrabajar['Importe'] = pd.to_numeric(
     dfPatrabajar['Importe'], errors='coerce')
 Resumen = dfPatrabajar.groupby(
-    ['Sucursal', 'TipoTarjeta','Moneda', 'Anio', 'Mes', 'Dia']).agg({'Importe': ['sum', 'count', 'median', 'mean', 'min', 'max']})
+    #estos erababan the original fields ouuuuu yeah
+    #['Sucursal', 'TipoTarjeta','Moneda', 'Anio', 'Mes', 'Dia']).agg({'Importe': ['sum', 'count', 'median', 'mean', 'min', 'max']})
+    #estos campos para cruzar info con el Juankiller
+    ['Anio', 'Mes', 'Dia']).agg({'Importe': ['sum', 'count', 'median', 'mean', 'min', 'max']})
 with pd.ExcelWriter(nombrecito + '.xlsx') as writer:  # pylint: disable=abstract-class-instantiated
     Resumen.to_excel(writer, sheet_name='Resumen') 
 print(dfPatrabajar.size)
